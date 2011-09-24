4db1650554d3c9a1429c96734437fd73d07fa014
zhaoming <zhaoming@hoperun.com>
2011-09-24 13:44:35 +0800

    [bug number  ]NEWMS00125077 [root cause  ]error commit [changes     ]remove a method [side effects]wifi Setting [reviewers   ]
    	modified:   core/java/android/preference/PreferenceCategory.java
    
    Change-Id: I88ca3ea566036e2548791f20de00d11079a1f2a7

--- core/java/android/preference/PreferenceCategory.java
+++ core/java/android/preference/PreferenceCategory.java
@@ -55,20 +55,4 @@ public class PreferenceCategory extends PreferenceGroup {
         return false;
     }
 
-    //add by Qian JianBo 2011-9-22 for NEWMS125077 begin
-    @Override
-    public Preference getPreference(int index) {
-    	Preference preference = null;
-        synchronized(this) {
-        	if (index < super.getPreferenceCount())
-        	{
-        		preference = super.getPreference(index);        		
-        	}
-        }
-        notifyHierarchyChanged();
-
-        return preference;
-    }
-    //add by Qian JianBo 2011-9-22 for NEWMS125077 end
-
 }
