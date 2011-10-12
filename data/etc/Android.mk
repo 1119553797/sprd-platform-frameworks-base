35a2052ceb6586f3bd29f7ce48b7c8d84493db98
gary.gong <gary.gong@spreadtrum.com>
2011-10-12 20:54:18 +0800

    fix android.app.cts.SystemFeaturesTest#testCameraFeatures
    Change-Id: I15c3d5251a8ee7df438c95d41297b5f6691ae5ad

--- data/etc/Android.mk
+++ data/etc/Android.mk
@@ -223,8 +223,26 @@ LOCAL_SRC_FILES := $(LOCAL_MODULE)
 
 include $(BUILD_PREBUILT)
 
+########################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := android.hardware.camera.flash-autofocus.xml
+
+LOCAL_MODULE_TAGS := user
+
+LOCAL_MODULE_CLASS := ETC
+
+# This will install the file in /system/etc/permissions
+#
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+
+include $(BUILD_PREBUILT)
 
 ########################
+########################
+
 #include $(CLEAR_VARS)
 
 #LOCAL_MODULE := required_hardware.xml
