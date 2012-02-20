057f2e5859bf098b0c85ff34fb5c2abe29b3d4fc
xiazhigen <xiazg0712@thundersoft.com>
2012-02-20 15:34:56 +0800

    ABSTRACT:change volume_FM Bug Fix [Y/N]:N Bug ID: New Feature [Y/N]:N Redmine Feature ID: Scratch or Copy [S/C]:S Copy from: Other Info:at connection Bluetooth time's change volume_fm
    Change-Id: I85a9ad7dd1613aff78a0167190d57c970e9fcca9

--- core/java/android/view/VolumePanel.java
+++ core/java/android/view/VolumePanel.java
@@ -200,13 +200,7 @@ public class VolumePanel extends Handler
 
             case AudioManager.STREAM_FM: {
                 message = FM_VOLUME_TEXT;
-                if (mAudioManager.isBluetoothA2dpOn()) {
-                    additionalMessage =
-                        com.android.internal.R.string.volume_music_hint_playing_through_bluetooth;
-                    setLargeIcon(com.android.internal.R.drawable.ic_volume_bluetooth_ad2p);
-                } else {
-                    setSmallIcon(index);
-                }
+                setSmallIcon(index);
                 break;
             }
 
