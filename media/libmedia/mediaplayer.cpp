0ae44e10e78a53f8a8bd8d9e008f3418b64d0bd8
apuser <andrew.zhang@spreadtrum.com>
2012-02-27 11:45:45 +0800

    [bug number ]fix bug when seeking can not pause [root cause ] [changes ] [side effects] [reviewers ]
    Change-Id: Ia5332be5e5f9170fe22044620be2c462ef3a97fe

--- media/libmedia/mediaplayer.cpp
+++ media/libmedia/mediaplayer.cpp
@@ -313,7 +313,7 @@ status_t MediaPlayer::pause()
     Mutex::Autolock _l(mLock);
     if (mCurrentState & (MEDIA_PLAYER_PAUSED|MEDIA_PLAYER_PLAYBACK_COMPLETE))
         return NO_ERROR;
-    if ((mPlayer != 0)&& (mSeekPosition < 0)&& (mCurrentState & MEDIA_PLAYER_STARTED)) {
+    if ((mPlayer != 0)&& (mCurrentState & MEDIA_PLAYER_STARTED)) {
         status_t ret = mPlayer->pause();
         if (ret != NO_ERROR) {
             mCurrentState = MEDIA_PLAYER_STATE_ERROR;
