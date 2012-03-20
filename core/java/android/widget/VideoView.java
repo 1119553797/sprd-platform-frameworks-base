408f95d045df6cf284102c2382649c3ea2a8b912
huangwf <huangwf@spreadst.com>
2012-03-20 14:02:20 +0800

    subject line
    [bug number  ] 13268
    [root cause  ] coding
    [changes     ] add judgement when mediaplayer is null
    [side effects] none
    [reviewers   ] zhaolq@spreadst.com
    
    	modified:   core/java/android/widget/VideoView.java
    
    Change-Id: I9be153c9d90452617d4b349697cd27c05dc98636

--- core/java/android/widget/VideoView.java
+++ core/java/android/widget/VideoView.java
@@ -830,18 +830,27 @@ public class VideoView extends SurfaceView implements MediaPlayerControl ,SetCan
 
     // added by MMz01
     public void setRightVolume(int rightVolume) {
+        if(mMediaPlayer == null){
+            return;
+        }
         float volumeRate = (float) (rightVolume * 1.0 / 15);
         mMediaPlayer.setVolume(0, volumeRate);
     }
 
     //added by MMz01
     public void setLeftVolume(int leftVolume){
+        if(mMediaPlayer == null){
+            return;
+        }
         float volumeRate = (float) (leftVolume * 1.0 / 15);
         mMediaPlayer.setVolume(volumeRate, 0);
     }
 
     //added by MMz01
     public void setBothVolume(int volume){
+        if(mMediaPlayer == null){
+            return;
+        }
         float volumeRate = (float) (volume * 1.0 / 15);
         mMediaPlayer.setVolume(volumeRate, volumeRate);
     }
