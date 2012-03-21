2e16e93e6dfb21eee648ca0b8b68c3bba9414406
tony.liu <tony.liu@spreadtrum.com>
2012-03-21 17:31:41 +0800

    support vids format 'H264'
    Change-Id: I40b3bd5f388f64929defed3a7e2fd854e90e58af

--- media/libstagefright/AVIExtractor.cpp
+++ media/libstagefright/AVIExtractor.cpp
@@ -670,6 +670,7 @@ static const char *GetMIMETypeForHandler(uint32_t handler) {
         case FOURCC('a', 'v', 'c', '1'):
         case FOURCC('d', 'a', 'v', 'c'):
         case FOURCC('h', '2', '6', '4'):
+        case FOURCC('H', '2', '6', '4'):
         case FOURCC('x', '2', '6', '4'):
         case FOURCC('v', 's', 's', 'h'):
             return MEDIA_MIMETYPE_VIDEO_AVC;
