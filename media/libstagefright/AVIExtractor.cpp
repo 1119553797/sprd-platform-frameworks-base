49099190f41a0670925669ad24e3b2c8ed53e2e5
tony.liu <tony.liu@spreadtrum.com>
2012-04-26 15:26:44 +0800

    support audio format 0x50
    Change-Id: Id66e8133606bdbdfc687b68656bb0ce1c48ef4f1

--- media/libstagefright/AVIExtractor.cpp
+++ media/libstagefright/AVIExtractor.cpp
@@ -822,7 +822,7 @@ status_t AVIExtractor::parseStreamFormat(off64_t offset, size_t size) {
     } else {
         uint32_t format = U16LE_AT(data);
 
-        if (format == 0x55) {
+        if ((format == 0x55) || (format == 0x50)) {
             track->mMeta->setCString(kKeyMIMEType, MEDIA_MIMETYPE_AUDIO_MPEG);
         } else if (format == 0x01) {
             track->mMeta->setCString(kKeyMIMEType, MEDIA_MIMETYPE_AUDIO_RAW);
