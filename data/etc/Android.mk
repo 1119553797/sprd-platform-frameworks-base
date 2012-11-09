8c5f6b845ddc652f7298ad68531cca41823175bf
changdongdong <changdd@spreadst.com>
2012-11-09 13:36:51 +0800

    Revert "bug#87004 <There are not BT and wifi settings items in the Settings interface.>"
    This reverts commit cd44b2dc342682de73fb25374b483ce8f8cd82d4
    Bugid is a wrong number, the corrcet should be 'bug#87044', and the build environment has been changed that result in this commit will not be build to the ROM.

--- data/etc/Android.mk
+++ data/etc/Android.mk
@@ -34,40 +34,6 @@ LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 ########################
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := android.hardware.bluetooth.xml
-
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_MODULE_CLASS := ETC
-
-# This will install the file in /system/etc/permissions
-#
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
-
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-
-include $(BUILD_PREBUILT)
-
-########################
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := android.hardware.wifi.xml
-
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_MODULE_CLASS := ETC
-
-# This will install the file in /system/etc/permissions
-#
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
-
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-
-include $(BUILD_PREBUILT)
-
-########################
 #include $(CLEAR_VARS)
 
 #LOCAL_MODULE := required_hardware.xml
