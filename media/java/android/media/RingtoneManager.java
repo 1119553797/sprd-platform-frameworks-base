332be245c014820e1d689da662fccc5664e13f76
zhanglei <zhanglei@spreadst.com>
2013-01-08 11:28:50 +0800

    bug#112985 description Background music lock screen length by side key, no reaction
    [bug number  ]112985
    [root cause  ]Background music lock screen length by side key, no reaction
    [changes     ]
    [side effects]none
    [reviewers   ]wanglx
    
    Change-Id: I56f342a176af6968d28a2a5cffac2a35cb0313f4

--- media/java/android/media/RingtoneManager.java
+++ media/java/android/media/RingtoneManager.java
@@ -405,7 +405,7 @@ public class RingtoneManager {
     public Uri getRingtoneUri(int position) {
         // use cursor directly instead of requerying it, which could easily
         // cause position to shuffle.
-        if (mCursor == null || !mCursor.moveToPosition(position)) {
+        if (mCursor == null || !mCursor.moveToPosition(position) || mCursor.isClosed()) {
             return null;
         }
         
