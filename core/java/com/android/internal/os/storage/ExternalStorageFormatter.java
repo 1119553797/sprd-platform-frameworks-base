6d281bafc385e8567939f86495cafd1cf8b1098e
JackyLiu <liujk@spreadst.com>
2013-03-04 13:25:22 +0800

    Bug#211605 [Storage] Modify ExternalStorageFormatter support format internal storage
    [Bug Number  ] 211605
    [Root Cause  ]
    [Changes     ] Modify ExternalStorageFormatter support format internal storage
    [Side Effects]
    [Reviewers   ] xiajing@spreadst.com huyp@spreadst.com xurz@spreadst.com
    [Feature ID  ]
    
    Change-Id: If886213646002ab759076a3a1980e4745e671b34

--- core/java/com/android/internal/os/storage/ExternalStorageFormatter.java
+++ core/java/com/android/internal/os/storage/ExternalStorageFormatter.java
@@ -7,6 +7,8 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Environment;
+// SPRD: add for add format internal storage
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.os.RemoteException;
@@ -21,6 +23,9 @@ import android.widget.Toast;
 
 import com.android.internal.R;
 
+// SPRD: add for add format internal storage
+import java.util.ArrayList;
+
 /**
  * Takes care of unmounting and formatting external storage.
  */
@@ -32,6 +37,12 @@ public class ExternalStorageFormatter extends Service
     public static final String FORMAT_AND_FACTORY_RESET = "com.android.internal.os.storage.FORMAT_AND_FACTORY_RESET";
 
     public static final String EXTRA_ALWAYS_RESET = "always_reset";
+    // SPRD: add for add format internal storage
+    // {@hide}
+    public static final String EXTRA_FORMAT_INTERNAL = "format_internal";
+    // SPRD: add for add format internal storage
+    // {@hide}
+    public static final String EXTRA_FORMAT_EXTERNAL = "format_external";
 
     // If non-null, the volume to format. Otherwise, will use the default external storage directory
     private StorageVolume mStorageVolume;
@@ -50,6 +61,48 @@ public class ExternalStorageFormatter extends Service
 
     private boolean mFactoryReset = false;
     private boolean mAlwaysReset = false;
+
+    /** SPRD: add for add format internal storage @{ */
+    private String mInternalStoragePath = null;
+    private String mExternalStoragePath = null;
+
+    private int mFormatStorageIndex = 0;
+    private int mFormatStorageCount = 0;
+    private StorageInfo mFormatStorageInfo = null;
+    private ArrayList<StorageInfo> mFormatStorageInfos
+            = new ArrayList<StorageInfo>();
+
+    private Handler mNextStorageHandler = null;
+
+    private static final int STORAGE_TYPE_INVALID = -1;
+    private static final int STORAGE_TYPE_INTERNAL = 0;
+    private static final int STORAGE_TYPE_EXTERNAL = 1;
+
+    class StorageInfo {
+        String mPath;
+        int mType;
+        public StorageInfo() {
+            mPath = null;
+            mType = STORAGE_TYPE_INVALID;
+        }
+        public StorageInfo(String path, int type) {
+            mPath = path;
+            mType = type;
+        }
+        public String toString() {
+            return "StorageInfo [ path="+mPath+", type="+mType+" ]";
+        }
+    };
+
+    private static final int MESSAGE_TYPE_UNMOUNTING = 0;
+    private static final int MESSAGE_TYPE_ERASING = 1;
+    private static final int MESSAGE_TYPE_FORMAT_ERROR = 2;
+    private static final int MESSAGE_TYPE_BAD_REMOVAL = 3;
+    private static final int MESSAGE_TYPE_CHECKING = 4;
+    private static final int MESSAGE_TYPE_REMOVED = 5;
+    private static final int MESSAGE_TYPE_SHARED = 6;
+    private static final int MESSAGE_TYPE_UNKNOWN_STATE = 7;
+    /** @} */
 
     // SPRD: add state
     private boolean mFinished = false;
@@ -83,8 +136,32 @@ public class ExternalStorageFormatter extends Service
         mWakeLock.acquire();
     }
 
+    /** SPRD: add for add format internal storage @{ */
+    private String getInternalStoragePath() {
+        if (mInternalStoragePath == null) {
+            mInternalStoragePath = Environment.getInternalStoragePath().getPath();
+        }
+
+        return mInternalStoragePath;
+    }
+
+    private String getExternalStoragePath() {
+        if (mExternalStoragePath == null) {
+            mExternalStoragePath = Environment.getExternalStoragePath().getPath();
+        }
+
+        return mExternalStoragePath;
+    }
+    /** @} */
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
+        /** SPRD: add for add format internal storage @{ */
+        boolean formatInternal = false;
+        boolean formatExternal = false;
+        /** @} */
+
         if (FORMAT_AND_FACTORY_RESET.equals(intent.getAction())) {
             mFactoryReset = true;
         }
@@ -95,6 +172,66 @@ public class ExternalStorageFormatter extends Service
         // SPRD: add state
         mFinished = false;
         mStorageVolume = intent.getParcelableExtra(StorageVolume.EXTRA_STORAGE_VOLUME);
+
+        /** SPRD: add for add format internal storage @{ */
+        formatInternal = intent.getBooleanExtra(EXTRA_FORMAT_INTERNAL, false);
+        formatExternal = intent.getBooleanExtra(EXTRA_FORMAT_EXTERNAL, false);
+
+        if (formatInternal || formatExternal) {
+            Log.d(TAG,"onStartCommand format : "+ (formatInternal ? "internal" : "")
+                + ((formatInternal & formatExternal) ? "," : "")
+                + (formatExternal ? "external" : ""));
+        }
+
+        if (mFormatStorageCount > 0) {
+            mFormatStorageInfos.clear();
+        }
+
+        String storagePath = null;
+        if (mStorageVolume != null) {
+            storagePath = mStorageVolume.getPath();
+            String internalStoragePath = getInternalStoragePath();
+            if (internalStoragePath != null && storagePath.equals(internalStoragePath)) {
+                mFormatStorageInfos.add(new StorageInfo(storagePath, STORAGE_TYPE_INTERNAL));
+            }
+            if (storagePath.equals(getExternalStoragePath())) {
+                mFormatStorageInfos.add(new StorageInfo(storagePath, STORAGE_TYPE_EXTERNAL));
+            }
+        }
+        if (formatInternal == false && formatExternal == false) {
+            if (mStorageVolume == null) {
+                mFormatStorageInfos.add(new StorageInfo(getExternalStoragePath(), STORAGE_TYPE_EXTERNAL));
+            }
+        } else {
+            if (formatInternal) {
+                String internalStoragePath = getInternalStoragePath();
+                if (internalStoragePath != null) {
+                    if (storagePath == null || !internalStoragePath.equals(storagePath)) {
+                        mFormatStorageInfos.add(new StorageInfo(internalStoragePath, STORAGE_TYPE_INTERNAL));
+                    }
+                }
+            }
+            if (formatExternal) {
+                String externalStoragePath = getExternalStoragePath();
+                if (externalStoragePath != null ) {
+                    if (storagePath == null || !externalStoragePath.equals(storagePath)) {
+                        mFormatStorageInfos.add(new StorageInfo(externalStoragePath, STORAGE_TYPE_EXTERNAL));
+                    }
+                }
+            }
+        }
+
+        mFormatStorageCount = mFormatStorageInfos.size();
+        mFormatStorageIndex = 0;
+        if (mFormatStorageCount > 0) {
+            mFormatStorageInfo = mFormatStorageInfos.get(0);
+            mNextStorageHandler = new Handler();
+        }
+
+        Log.d(TAG,"onStartCommand mFormatStorageCount is " + mFormatStorageCount);
+        for (int index = 0; index < mFormatStorageCount; index++)
+            Log.d(TAG,"onStartCommand mFormatStorageInfos["+index+"] is " + mFormatStorageInfos.get(index));
+        /** @} */
 
         if (mProgressDialog == null) {
             mProgressDialog = new ProgressDialog(this);
@@ -131,11 +268,19 @@ public class ExternalStorageFormatter extends Service
     @Override
     public void onCancel(DialogInterface dialog) {
         IMountService mountService = getMountService();
+        /** SPRD: modified for add format internal storage @{
+          @orig
         String extStoragePath = mStorageVolume == null ?
                 Environment.getLegacyExternalStorageDirectory().toString() :
                 mStorageVolume.getPath();
         try {
             mountService.mountVolume(extStoragePath);
+        */
+        try {
+            mFormatStorageIndex = mFormatStorageCount;
+            if (mFormatStorageInfo != null)
+                mountService.mountVolume(mFormatStorageInfo.mPath);
+        /** @} */
         } catch (RemoteException e) {
             Log.w(TAG, "Failed talking with mount service", e);
         }
@@ -144,52 +289,180 @@ public class ExternalStorageFormatter extends Service
 
     void fail(int msg) {
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+        /** SPRD: modified for add format internal storage @{
+          @orig
         if (mAlwaysReset) {
             sendBroadcast(new Intent("android.intent.action.MASTER_CLEAR"));
         }
+        */
+        nextStorageOrFactoryReset(mAlwaysReset);
+        /** @} */
         stopSelf();
     }
 
+    /*
+     * SPRD: add for add format internal storage @{
+     *
+     * return if has next storage need to format
+     */
+    private boolean nextStorageOrFactoryReset(boolean factoryReset) {
+        mFormatStorageIndex++;
+        if (mFormatStorageIndex < mFormatStorageCount) {
+            mNextStorageHandler.post(new Runnable(){
+                public void run() {
+                    updateProgressState();
+                }
+            });
+            return true;
+        }
+        if (factoryReset) {
+            sendBroadcast(new Intent("android.intent.action.MASTER_CLEAR"));
+        }
+        return false;
+    }
+
+    private int getMessageID(int messageType) {
+        int storageType = STORAGE_TYPE_EXTERNAL;
+        if (mFormatStorageInfo != null) {
+            storageType = mFormatStorageInfo.mType;
+        }
+        if (storageType == STORAGE_TYPE_INTERNAL) {
+            switch(messageType) {
+                case MESSAGE_TYPE_UNMOUNTING:
+                    return R.string.progress_unmounting1;
+                case MESSAGE_TYPE_ERASING:
+                    return R.string.progress_erasing1;
+                case MESSAGE_TYPE_FORMAT_ERROR:
+                    return R.string.format_error1;
+                case MESSAGE_TYPE_BAD_REMOVAL:
+                    return R.string.media_bad_removal1;
+                case MESSAGE_TYPE_CHECKING:
+                    return R.string.media_checking1;
+                case MESSAGE_TYPE_REMOVED:
+                    return R.string.media_removed1;
+                case MESSAGE_TYPE_SHARED:
+                    return R.string.media_shared1;
+                case MESSAGE_TYPE_UNKNOWN_STATE:
+                    return R.string.media_unknown_state1;
+            }
+        } else {
+            switch(messageType) {
+                case MESSAGE_TYPE_UNMOUNTING:
+                    return R.string.progress_unmounting;
+                case MESSAGE_TYPE_ERASING:
+                    return R.string.progress_erasing;
+                case MESSAGE_TYPE_FORMAT_ERROR:
+                    return R.string.format_error;
+                case MESSAGE_TYPE_BAD_REMOVAL:
+                    return R.string.media_bad_removal;
+                case MESSAGE_TYPE_CHECKING:
+                    return R.string.media_checking;
+                case MESSAGE_TYPE_REMOVED:
+                    return R.string.media_removed;
+                case MESSAGE_TYPE_SHARED:
+                    return R.string.media_shared;
+                case MESSAGE_TYPE_UNKNOWN_STATE:
+                    return R.string.media_unknown_state;
+            }
+        }
+        return R.string.untitled;
+    }
+    /** @} */
+
     void updateProgressState() {
+        /** SPRD: modified for add format internal storage @{
+         @orig
         String status = mStorageVolume == null ?
                 Environment.getExternalStorageState() :
                 mStorageManager.getVolumeState(mStorageVolume.getPath());
+        */
+        if (mFormatStorageIndex < mFormatStorageCount) {
+            mFormatStorageInfo = mFormatStorageInfos.get(mFormatStorageIndex);
+        } else {
+            if (mFormatStorageCount == 0 && (mFactoryReset || mAlwaysReset)) {
+                sendBroadcast(new Intent("android.intent.action.MASTER_CLEAR"));
+            }
+            stopSelf();
+            return;
+        }
+        final String formatStoragePath = mFormatStorageInfo.mPath;
+        String status = mStorageManager.getVolumeState(formatStoragePath);
+        /** @} */
         if (Environment.MEDIA_MOUNTED.equals(status)
                 || Environment.MEDIA_MOUNTED_READ_ONLY.equals(status)) {
+            /** SPRD: modified for add format internal storage @{
+             @orig
             updateProgressDialog(R.string.progress_unmounting);
+            */
+            updateProgressDialog(getMessageID(MESSAGE_TYPE_UNMOUNTING));
+            /** @} */
             IMountService mountService = getMountService();
+            /** SPRD: delete for add format internal storage
+             @orig
             final String extStoragePath = mStorageVolume == null ?
                     Environment.getLegacyExternalStorageDirectory().toString() :
                     mStorageVolume.getPath();
+            */
             try {
                 // Remove encryption mapping if this is an unmount for a factory reset.
+                /** SPRD: modified for add format internal storage @{
+                 @orig
                 mountService.unmountVolume(extStoragePath, true, mFactoryReset);
+                */
+                mountService.unmountVolume(formatStoragePath, true, mFactoryReset);
+                /** @} */
             } catch (RemoteException e) {
                 Log.w(TAG, "Failed talking with mount service", e);
             }
         } else if (Environment.MEDIA_NOFS.equals(status)
                 || Environment.MEDIA_UNMOUNTED.equals(status)
                 || Environment.MEDIA_UNMOUNTABLE.equals(status)) {
+            /** SPRD: modified for add format internal storage @{
+             @orig
             updateProgressDialog(R.string.progress_erasing);
+            */
+            updateProgressDialog(getMessageID(MESSAGE_TYPE_ERASING));
+            /** @} */
             final IMountService mountService = getMountService();
+            /** SPRD: delete for add format internal storage
+             @orig
             final String extStoragePath = mStorageVolume == null ?
                     Environment.getLegacyExternalStorageDirectory().toString() :
                     mStorageVolume.getPath();
+            */
             if (mountService != null) {
                 new Thread() {
                     @Override
                     public void run() {
                         boolean success = false;
                         try {
+                            /** SPRD: modified for add format internal storage @{
+                             @orig
                             mountService.formatVolume(extStoragePath);
+                             */
+                            mountService.formatVolume(formatStoragePath);
+                            /** @} */
                             success = true;
                         } catch (Exception e) {
+                            /** SPRD: modified for add format internal storage @{
+                             @orig
                             Toast.makeText(ExternalStorageFormatter.this,
                                     R.string.format_error, Toast.LENGTH_LONG).show();
+                             */
+                            Toast.makeText(ExternalStorageFormatter.this,
+                                    getMessageID(MESSAGE_TYPE_FORMAT_ERROR), Toast.LENGTH_LONG).show();
+                            /** @} */
                         }
                         if (success) {
                             if (mFactoryReset) {
+                                /** SPRD: modified for add format internal storage @{
+                                 @orig
                                 sendBroadcast(new Intent("android.intent.action.MASTER_CLEAR"));
+                                 */
+                                if (nextStorageOrFactoryReset(true)) {
+                                    return;
+                                }
+                                /** @} */
                                 // Intent handling is asynchronous -- assume it will happen soon.
                                 stopSelf();
                                 return;
@@ -198,10 +471,20 @@ public class ExternalStorageFormatter extends Service
                         // If we didn't succeed, or aren't doing a full factory
                         // reset, then it is time to remount the storage.
                         if (!success && mAlwaysReset) {
+                            /** SPRD: modified for add format internal storage @{
+                             @orig
                             sendBroadcast(new Intent("android.intent.action.MASTER_CLEAR"));
+                             */
+                            nextStorageOrFactoryReset(true);
+                            /** @} */
                         } else {
                             try {
+                                /** SPRD: modified for add format internal storage @{
+                                 @orig
                                 mountService.mountVolume(extStoragePath);
+                                 */
+                                mountService.mountVolume(formatStoragePath);
+                                /** @} */
                                 // SPRD: set finish flag to avoid handling mount state again
                                 mFinished = true;
                             } catch (RemoteException e) {
@@ -216,17 +499,45 @@ public class ExternalStorageFormatter extends Service
                 Log.w(TAG, "Unable to locate IMountService");
             }
         } else if (Environment.MEDIA_BAD_REMOVAL.equals(status)) {
+            /** SPRD: modified for add format internal storage @{
+             @orig
             fail(R.string.media_bad_removal);
+            */
+            fail(getMessageID(MESSAGE_TYPE_BAD_REMOVAL));
+            /** @} */
         } else if (Environment.MEDIA_CHECKING.equals(status)) {
+            /** SPRD: modified for add format internal storage @{
+             @orig
             fail(R.string.media_checking);
+            */
+            fail(getMessageID(MESSAGE_TYPE_CHECKING));
+            /** @} */
         } else if (Environment.MEDIA_REMOVED.equals(status)) {
+            /** SPRD: modified for add format internal storage @{
+             @orig
             fail(R.string.media_removed);
+            */
+            fail(getMessageID(MESSAGE_TYPE_REMOVED));
+            /** @} */
         } else if (Environment.MEDIA_SHARED.equals(status)) {
+            /** SPRD: modified for add format internal storage @{
+             @orig
             fail(R.string.media_shared);
+            */
+            fail(getMessageID(MESSAGE_TYPE_SHARED));
+            /** @} */
         } else {
+            /** SPRD: modified for add format internal storage @{
+             @orig
             fail(R.string.media_unknown_state);
+            */
+            fail(getMessageID(MESSAGE_TYPE_UNKNOWN_STATE));
+            /** @} */
             Log.w(TAG, "Unknown storage state: " + status);
+            /** SPRD: delete for add format internal storage
+             @orig
             stopSelf();
+            */
         }
     }
 
