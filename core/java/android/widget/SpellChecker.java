5ab9655c60ffa08dc4beb61aeafa32500a2643e9
hill wang <hill.wang@spreadtrum.com>
2013-05-27 15:35:35 +0800

    bug#161957 com.sprdroid.note JAVAcrash [bug number  ]161957 [root cause  ]wordStart > end [changes     ] [side effects] [reviewers   ]mengya.liu luther.ge ace.an
    Change-Id: I82d15355c5d54febbf2c39ffdb80342934efeeba

--- core/java/android/widget/SpellChecker.java
+++ core/java/android/widget/SpellChecker.java
@@ -728,7 +728,7 @@ public class SpellChecker implements SpellCheckerSessionListener {
                 }
             }
 
-            if (scheduleOtherSpellCheck) {
+            if (scheduleOtherSpellCheck && (wordStart < end)) {
                 // Update range span: start new spell check from last wordStart
                 setRangeSpan(editable, wordStart, end);
             } else {
