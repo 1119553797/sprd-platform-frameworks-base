50e5a0da1e356e0fa63b085e6fc77ddc97b1b727
zhuo.yan <zhuo.yan@spreadtrum.com>
2013-05-31 18:00:31 +0800

    Bug#168833, Enter recipient first comma, the display is P
    Change-Id: I046d3e9fb1883ac85266ae839728a37fefe3481c

--- telephony/java/android/telephony/PhoneNumberFormattingTextWatcher.java
+++ telephony/java/android/telephony/PhoneNumberFormattingTextWatcher.java
@@ -135,7 +135,8 @@ public class PhoneNumberFormattingTextWatcher implements TextWatcher {
         boolean hasCursor = false;
         int len = s.length();
         for (int i = 0; i < len; i++) {
-            char c = PhoneNumberUtils.pAndwToCommaAndSemicolon (s.charAt(i));
+ //           char c = PhoneNumberUtils.pAndwToCommaAndSemicolon (s.charAt(i));
+            char c = s.charAt(i);
             if (PhoneNumberUtils.isNonSeparator(c)) {
                 if (lastNonSeparator != 0) {
                     formatted = getFormattedNumber(lastNonSeparator, hasCursor);
@@ -150,7 +151,8 @@ public class PhoneNumberFormattingTextWatcher implements TextWatcher {
         if (lastNonSeparator != 0) {
             formatted = getFormattedNumber(lastNonSeparator, hasCursor);
         }
-        return PhoneNumberUtils.CommaAndSemicolonTopAndw(formatted);
+//        return PhoneNumberUtils.CommaAndSemicolonTopAndw(formatted);
+        return formatted;
     }
 
     private String getFormattedNumber(char lastNonSeparator, boolean hasCursor) {
