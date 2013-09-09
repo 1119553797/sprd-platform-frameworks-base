339d7c29dc2e15179a8175f130ce92c7c82f3358
quqixuan <quqx@spreadst.com>
2013-09-09 14:56:09 +0800

    bug#213602 The shutdown interface into the USB ,dialog disappears.
    [bug number ]213602
    [root cause ]Call dialog confict with the low battery.
    [changes ]Add distinguish parameters.
    [side effects]
    [reviewers ]xurz@spreadst.com

--- services/java/com/android/server/BatteryService.java
+++ services/java/com/android/server/BatteryService.java
@@ -256,6 +256,8 @@ public final class BatteryService extends Binder {
                         Intent intent = new Intent(Intent.ACTION_REQUEST_SHUTDOWN);
                         intent.putExtra(Intent.EXTRA_KEY_CONFIRM, false);
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        // SPRD: Add can_be_cancel attributes to distinguish between low-battery shutdown.
+                        intent.putExtra("can_be_cancel", true);
                         mContext.startActivityAsUser(intent, UserHandle.CURRENT);
                     }
                 }
