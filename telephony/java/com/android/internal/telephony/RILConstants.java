db88d5b58c9d8ed3867af31e7d2f81cbbea9eeb1
cheng.luo <cheng.luo@spreadtrum.com>
2013-09-29 19:43:16 -0400

    bug#219009 no noise when switch handfree in call
    [bug number ]219009
    [root cause ]
    [changes ]
    [side effects]
    [reviewers ]
    
    Change-Id: Idc1f51ec4cef1958a0925c6fc275bf8156550c56

--- telephony/java/com/android/internal/telephony/RILConstants.java
+++ telephony/java/com/android/internal/telephony/RILConstants.java
@@ -300,4 +300,8 @@ cat include/telephony/ril.h | \
     int RIL_UNSOL_RIL_CONNECTED = 1034;
     int RIL_UNSOL_VOICE_RADIO_TECH_CHANGED = 1035;
     int RIL_UNSOL_CELL_INFO_LIST = 1036;
+	
+	 /* SPRD: add for sprd ril @{ */
+	int RIL_SPRD_REQUEST_BASE = 5000;
+	int RIL_REQUEST_SEND_AT = RIL_SPRD_REQUEST_BASE + 29;//SPRD: add
 }
